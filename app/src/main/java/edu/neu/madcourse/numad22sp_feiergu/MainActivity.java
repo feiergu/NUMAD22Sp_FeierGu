90caad1cbabc9b96f793e4fee78c702f045683d7
feiergu <gu.fei@northeastern.edu>
2022-01-21 22:53:00 -0800

    Completed A1.

--- app/src/main/java/edu/neu/madcourse/numad22sp_feiergu/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_feiergu/MainActivity.java
@@ -3,8 +3,21 @@ package edu.neu.madcourse.numad22sp_feiergu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("About me", "Button pressed!");
+        Toast.makeText(this, "Feier Gu\ngu.fei@northeastern.edu", Toast.LENGTH_SHORT).show();
+    }
+
+    public void greetFunction(View view) {
+        Log.i("Greet", "Button pressed!");
+        Toast.makeText(this, "Have a nice day!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

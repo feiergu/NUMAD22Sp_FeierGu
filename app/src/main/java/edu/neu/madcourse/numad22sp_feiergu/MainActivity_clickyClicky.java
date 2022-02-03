5cef733a0d085b2bd4932b927bfdbf0743845ca4
feiergu <gu.fei@northeastern.edu>
2022-02-02 16:53:25 -0800

    Added buttons and textview on clicky clicky activity.

--- app/src/main/java/edu/neu/madcourse/numad22sp_feiergu/MainActivity_clickyClicky.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_feiergu/MainActivity_clickyClicky.java
@@ -3,6 +3,10 @@ package edu.neu.madcourse.numad22sp_feiergu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity_clickyClicky extends AppCompatActivity {
 

441f32e674e74e443bbaaf3be794ed912287dac8
feiergu <gu.fei@northeastern.edu>
2022-02-17 19:17:52 -0800

    Completed displayLocation activity.

--- app/src/main/java/edu/neu/madcourse/numad22sp_feiergu/MainActivity_displayLocation.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_feiergu/MainActivity_displayLocation.java
@@ -1,14 +1,105 @@
 package edu.neu.madcourse.numad22sp_feiergu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity_displayLocation extends AppCompatActivity {
+
+    TextView textView_latitude_content;
+    TextView textView_longitude_content;
+    //Button button;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_display_location);
+
+        textView_latitude_content = findViewById(R.id.textView_latitude_content);
+        textView_longitude_content = findViewById(R.id.textView_longitude_content);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity_displayLocation.this);
+        // check runtime permissions
+        if (ActivityCompat.checkSelfPermission(MainActivity_displayLocation.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(MainActivity_displayLocation.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity_displayLocation.this, new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 1);
+        } else {
+            getLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0) {
+            if (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                getLocation();
+            } else {
+                Toast.makeText(getApplicationContext(), "Permission denied!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            // check if gps is enabled
+            Toast.makeText(getApplicationContext(), "Please allow access to location.", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+        } else {
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        textView_latitude_content.setText(String.valueOf(location.getLatitude()));
+                        textView_longitude_content.setText(String.valueOf(location.getLongitude()));
+                    } else {
+                        // send location request
+                        LocationRequest locationRequest = new LocationRequest();
+                        locationRequest.setInterval(10000);
+                        locationRequest.setFastestInterval(3000);
+                        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                        locationRequest.setNumUpdates(1);
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(@NonNull LocationResult locationResult) {
+                                super.onLocationResult(locationResult);
+                                Location location = locationResult.getLastLocation();
+                                textView_latitude_content.setText(String.valueOf(location.getLatitude()));
+                                textView_longitude_content.setText(String.valueOf(location.getLongitude()));
+                            }
+                        };
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                    }
+                }
+            });
+        }
     }
 }
